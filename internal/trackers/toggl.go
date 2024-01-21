cfbf1d83052e5996727feddf9dec8391d152d818
Cristiano Amorim <cristianoretiro2003@gmail.com>
2024-01-21 16:03:32 -0300

    Create stop entry command

--- internal/trackers/toggl.go
+++ internal/trackers/toggl.go
@@ -82,3 +82,78 @@ func (t *Toggl) Start(description string) (string, error) {
 	defer resp.Body.Close()
 	return fmt.Sprintf("Time entry started at %s: %s", start, description), nil
 }
+
+type TimeEntry struct {
+	Id              int        `json:"id"`
+	WorkspaceId     int        `json:"workspace_id"`
+	ProjectId       *int       `json:"project_id"`
+	TaskId          *int       `json:"task_id"`
+	Billable        bool       `json:"billable"`
+	Start           time.Time  `json:"start"`
+	Stop            *time.Time `json:"stop"`
+	Duration        int        `json:"duration"`
+	Description     string     `json:"description"`
+	Tags            []string   `json:"tags"`
+	TagIds          []int      `json:"tag_ids"`
+	Duronly         bool       `json:"duronly"`
+	At              time.Time  `json:"at"`
+	ServerDeletedAt *time.Time `json:"server_deleted_at"`
+	UserId          int        `json:"user_id"`
+	Uid             int        `json:"uid"`
+	Wid             int        `json:"wid"`
+}
+
+func (t *Toggl) getCurrentEntry() (*TimeEntry, error) {
+	url := "https://api.track.toggl.com/api/v9/me/time_entries/current"
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error: %s", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	username := t.ApiToken
+	password := "api_token"
+	req.SetBasicAuth(username, password)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error: %s", err)
+	}
+	var body TimeEntry
+	err = json.NewDecoder(resp.Body).Decode(&body)
+	if err != nil {
+		return nil, fmt.Errorf("error: %s", err)
+	}
+	defer resp.Body.Close()
+	return &body, nil
+}
+
+func (t *Toggl) Stop() (string, error) {
+	t.Setup()
+	c, err := t.getCurrentEntry()
+	if err != nil {
+		return "", fmt.Errorf("error: %s", err)
+	}
+	if c.Id == 0 {
+		return "", fmt.Errorf("no time entry running, please start tracking")
+	}
+	url := fmt.Sprintf("https://api.track.toggl.com/api/v9/workspaces/%d/time_entries/%d/stop", t.WorkspaceId, c.Id)
+	client := &http.Client{}
+	req, err := http.NewRequest("PATCH", url, nil)
+	if err != nil {
+		return "", fmt.Errorf("error: %s", err)
+	}
+	username := t.ApiToken
+	password := "api_token"
+	req.SetBasicAuth(username, password)
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("error: %s", err)
+	}
+	var s TimeEntry
+	err = json.NewDecoder(resp.Body).Decode(&s)
+	if err != nil {
+		return "", fmt.Errorf("error: %s", err)
+	}
+	parsedTime := s.Stop.Local().Format(time.RFC3339)
+	return fmt.Sprintf("Time entry stopped at %s: %s", parsedTime, c.Description), nil
+}
